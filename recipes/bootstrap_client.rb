bb76d3282b4ba133e421c637dffc20961471eae1
Greg Farnum <greg@inktank.com>
2012-08-02 15:29:12 -0700

    create a ceph-client role
    The ceph-client role sets the host up to access the Ceph
    cluster and generates the host a key capable of creating
    new client access keys

--- recipes/bootstrap_client.rb
+++ recipes/bootstrap_client.rb
@@ -3,20 +3,10 @@
 include_recipe "ceph::default"
 include_recipe "ceph::conf"
 
-mons = nil
-
-if is_crowbar?
-  # for now, just assume the first proposal is the right one if we aren't assigned one
-  # TODO: this is a dirty, dirty hack because I don't want to work out the proper search
-  ceph_proposal = node['nova']['ceph_instance']
-  ceph_cluster_name = data_bag_item("crowbar", "bc-ceph-#{ceph_proposal}")["deployment"]["ceph"]["config"]["environment"]
-  mons = get_mon_nodes(ceph_cluster_name, "ceph_bootstrap_client_key:*")
-else
-  mons = get_mon_nodes(node['ceph']['config']['environment'], "ceph_bootstrap_client_key:*")
-end
+mons = get_mon_nodes("ceph_bootstrap_client_key:*")
 
 if mons.empty? then
-  Chef::Log.info("No ceph-mon having ceph_bootstrap_client_key found.")
+  puts "No ceph-mon having ceph_bootstrap_client_key found."
 else
   
   directory "/var/lib/ceph/bootstrap-client" do
@@ -28,13 +18,20 @@ else
   #TODO cluster name
   cluster = 'ceph'
 
+  file "/var/lib/ceph/bootstrap-client/#{cluster}.keyring.raw" do
+    owner "root"
+    group "root"
+    mode "0440"
+    content mons[0]["ceph_bootstrap_client_key"]
+  end
+
   execute "format as keyring" do
     command <<-EOH
       set -e
       # TODO don't put the key in "ps" output, stdout
-      ceph-authtool '/var/lib/ceph/bootstrap-client/#{cluster}.keyring' --create-keyring --name=client.bootstrap-client --add-key='#{mons[0]["ceph_bootstrap_client_key"]}'
+      read KEY <'/var/lib/ceph/bootstrap-client/#{cluster}.keyring.raw'
+      ceph-authtool '/var/lib/ceph/bootstrap-client/#{cluster}.keyring' --create-keyring --name=client.bootstrap-client --add-key="$KEY"
       rm -f '/var/lib/ceph/bootstrap-client/#{cluster}.keyring.raw'
     EOH
-    creates 'var/lib/ceph/bootstrap-client/#{cluster}.keyring'
   end
 end
