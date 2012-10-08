64102607f1d151bdb34743733dc05b6d173479fe
Guilhem Lettron <guilhem@lettron.fr>
2012-10-08 18:39:07 +0200

    big cleanup Only change behaviour of node['ceph_bootstrap_client_key'] and node['ceph_bootstrap_osd_key'] to be more generic Must be test

--- recipes/mon.rb
+++ recipes/mon.rb
@@ -21,6 +21,7 @@ end
 
 # TODO cluster name
 cluster = node['ceph']['cluster']
+mon_addresses = get_mon_addresses()
 
 execute 'ceph-mon mkfs' do
   command <<-EOH
@@ -40,17 +41,15 @@ end
 
 ruby_block "tell ceph-mon about its peers" do
   block do
-    mon_addresses = get_mon_addresses()
     mon_addresses.each do |addr|
-      system 'ceph', \
-        '--admin-daemon', "/var/run/ceph/#{cluster}-mon.#{node['hostname']}.asok", \
-        'add_bootstrap_peer_hint', addr
-      # ignore errors
+      e = Chef::Resource::Execute.new ("tell ceph-mon #{addr} about its peers", run_context)
+      e.command = "ceph --admin-daemon /var/run/ceph/#{cluster}-mon.#{node['hostname']}.asok add_bootstrap_peer_hint #{addr}"
+      r.run_action(:run)
     end
   end
 end
 
-have_key = ::File.exists?('/etc/ceph/#{cluster}.client.admin.keyring')
+have_key = ::File.exists?("/etc/ceph/#{cluster}.client.admin.keyring")
 
 ruby_block "wait until quorum is formed" do
   block do
@@ -63,15 +62,11 @@ end
 
 ruby_block "create client.admin keyring" do
   block do
-    if not have_key then
-      if not have_quorum? then
-        Chef::Log.info('ceph-mon is not in quorum, skipping bootstrap-osd key generation for this run')
-      else
-        # TODO --set-uid=0
-        key = %x[
-        ceph \
-          --name mon. \
-          --keyring '/var/lib/ceph/mon/#{cluster}-#{node['hostname']}/keyring' \
+    # TODO --set-uid=0
+    key = %x[
+    ceph \
+      --name mon. \
+          --keyring "/var/lib/ceph/mon/#{cluster}-#{node['hostname']}/keyring" \
           auth get-or-create-key client.admin \
           mon 'allow *' \
           osd 'allow *' \
@@ -80,24 +75,19 @@ ruby_block "create client.admin keyring" do
         raise 'adding or getting admin key failed' unless $?.exitstatus == 0
         # TODO don't put the key in "ps" output, stdout
         system 'ceph-authtool', \
-          '/etc/ceph/#{cluster}.client.admin.keyring', \
+          "/etc/ceph/#{cluster}.client.admin.keyring", \
           '--create-keyring', \
           '--name=client.admin', \
           "--add-key=#{key}"
         raise 'creating admin keyring failed' unless $?.exitstatus == 0
-      end
-    end
   end
+  not_if { have_key }
+  only_if { have_quorum? }
 end
 
-ruby_block "save bootstrap keys in node attributes" do
+ruby_block "save bootstrap_osd_key" do
   block do
-    if node['ceph_bootstrap_osd_key'].nil? then
-      raise "missing bootstrap_osd key but do have bootstrap_client key!" unless node['ceph_bootstrap_client_key'].nil?
-      if not have_quorum? then
-        Chef::Log.info('ceph-mon is not in quorum, skipping bootstrap key generation for this run')
-      else
-        osd_key = %x[
+    osd_key = %x[
           ceph \
             --name mon. \
             --keyring '/var/lib/ceph/mon/#{cluster}-#{node['hostname']}/keyring' \
@@ -107,24 +97,26 @@ ruby_block "save bootstrap keys in node attributes" do
             allow command auth add * osd allow\\ * mon allow\\ rwx; \
             allow command mon getmap"
         ]
-        raise 'adding or getting bootstrap-osd key failed' unless $?.exitstatus == 0
-        node.override['ceph_bootstrap_osd_key'] = osd_key
+    raise 'adding or getting bootstrap-osd key failed' unless $?.exitstatus == 0
+    node.set['ceph']['bootstrap_osd_key'] = osd_key
+  end
+  only_if { have_quorum? }
+  not_if { node['ceph']['bootstrap_osd_key'] }
+end
 
-        client_key = %x[
+ruby_block "save bootstrap_client_key" do
+  block do
+    client_key = %x[
           ceph \
             --name mon. \
             --keyring '/var/lib/ceph/mon/#{cluster}-#{node['hostname']}/keyring' \
             auth get-or-create-key client.bootstrap-client mon \
             "allow command auth get-or-create-key * osd * mon *;"
         ]
-        raise 'adding or getting bootstrap-client key failed' unless $?.exitstatus == 0
-        node.override['ceph_bootstrap_client_key'] = client_key
-        
-        node.save
-      end
-    else #node['ceph_bootstrap_osd_key'] not nil
-      raise "have ceph_bootstrap_osd_key but not bootstrap_client key!" unless !node['ceph_bootstrap_client_key'].nil?
-    end
+    raise 'adding or getting bootstrap-client key failed' unless $?.exitstatus == 0
+    node.set['ceph']['bootstrap_client_key'] = client_key
+    node.save
   end
+  only_if { have_quorum? }
+  not_if { node['ceph']['bootstrap_client_key'] }
 end
-
